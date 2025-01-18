e708a69c4f7097a2c585d8f3b8daa1ef0abf22c2
Essyuge <ugeesther@gmail.com>
2025-01-18 09:48:17 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get "sessions/create"
+  get "sessions/destroy"
+  resources :tickets
+  resources :users
+  resources :cast_members
+  resources :productions
+
+  post "/login", to: "sessions#create"
+
+  delete "/logout", to: "sessions#destroy"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
